d0e91a828e2b578b06affdb389e3ff8af93ae97d
Yuri Mello <yuri.mcr@gmail.com>
2012-06-27 20:27:16 -0300

    Inicializando repositório

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SucurilabsWebsite::Application.config.secret_token = '031157f14f145b935d9a3826d6330654f45ad15956ae3dfaaeea93066b539077d2dd5678c153888751dd463f487441f15baef3d93b030a24c9bd4a92ce71d9f2'
