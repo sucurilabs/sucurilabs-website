eefcd4a0e24157c8d1f6aa43e37468eb91a19088
Yuri Mello <yuri.mcr@gmail.com>
2012-06-30 18:49:43 -0300

    Configurando deploy

--- config.ru
+++ config.ru
@@ -1,9 +1,3 @@
-ENV['GEM_HOME']="#{ENV['HOME']}/.rvm/gems/ruby-1.9.3-p194@global"
-ENV['GEM_PATH']="#{ENV['GEM_HOME']}"
-#require 'rubygems'
-Gem.clear_paths
-# This file is used by Rack-based servers to start the application.
-
 require ::File.expand_path('../config/environment',  __FILE__)
 
 run SucurilabsWebsite::Application
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,29 +1,59 @@
-set :user, 'yurimelloc'  # Your dreamhost account's username
-set :domain, 'sucurilabs.net'  # Dreamhost servername where your account is located 
-set :project, 'sucurilabs-website'  # Your application as its called in the repository
-set :application, 'sucurilabs.net'  # Your app's location (domain or sub-domain name as setup in panel)
-set :applicationdir, "/home/#{user}/#{application}"  # The standard Dreamhost setup
+#this part goes in the deploy.rb file inside the config in your rails app
 
-# version control config
-set :scm, 'git'
-set :repository,  "git@github.com:sucurilabs/sucurilabs-website.git"
-set :deploy_via, :remote_cache
-set :git_enable_submodules, 1 # if you have vendored rails
-set :branch, 'master'
-set :git_shallow_clone, 1
-set :scm_verbose, true
+require 'bundler/capistrano'
+set :application, "sucurilabs.net"
 
-# roles (servers)
-role :web, domain
+set :domain, "sucurilabs.net"
+set :environment, "production"
+set :branch, "master"
+set :deploy_to, "/var/www/#{aplication}"
+
 role :app, domain
-role :db,  domain, :primary => true
+role :web, domain
+role :db, domain, :primary => true
 
-# deploy config
-set :deploy_to, applicationdir
-set :deploy_via, :export
+default_run_options[:pty] = true
 
-# additional settings
-default_run_options[:pty] = true  # Forgo errors when deploying from windows
-#ssh_options[:keys] = %w(/Path/To/id_rsa)            # If you are using ssh_keys
-set :chmod755, "app config db lib public vendor script script/* public/disp*"
+default_run_options[:shell] = 'bash'
+
+default_environment["RAILS_ENV"] = 'production'
+
+set :repository, "git@github.com:sucurilabs/sucurilabs-website.git"
+set :deploy_via, :remote_cache
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+set :scm_verbose, true
 set :use_sudo, false
+set :ssh_options, :forward_agent => true
+
+set :user, "deployerbot"
+set :keep_releases, 5
+
+set :default_environment, {
+    'PATH' => "/usr/local/rvm/gems/ruby-1.9.3-p194@sucurilabs-website/gems/bin:$PATH",
+    'RUBY_VERSION' => 'ruby 1.9.3',
+    'GEM_HOME' => '/usr/local/rvm/gems/ruby-1.9.3-p194@sucurilabs-website/gems',
+    'GEM_PATH' => '/usr/local/rvm/gems/ruby-1.9.3-p194@sucurilabs-website/gems:/usr/local/rvm/gems/ruby-1.9.3-p194@global/gems',
+    'BUNDLE_PATH' => '//usr/local/rvm/gems/ruby-1.9.3-p194@sucurilabs-website/gems' # If using bundler.
+}
+
+namespace :deploy do
+  desc "restarting"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "symlink vendor to shared vendor"
+  task :symlink_vendor_to_shared_vendor do
+    run "ln -s #{current_path}/../shared/vendor #{current_path}/vendor"
+  end
+
+  desc "trust rvmrc"
+  task :trust_rvmrc do
+    run "rvm rvmrc trust #{release_path}"
+  end
+end
+
+after 'deploy:symlink', 'deploy:symlink_vendor_to_shared_vendor', 'deploy:trust_rvmrc'
